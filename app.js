3f9740e1f4d5d67bc5b31921a1f9440eb482a7bf
Simen Haugerud Granlund <simen@hgranlund.com>
2013-02-06 22:59:56 +0100

    stated on scale image

--- app.js
+++ app.js
@@ -13,6 +13,38 @@ console.log('Listening on port 3000');
 var io = socket.listen(server);
 
 io.sockets.on('connection', function(socket) {
+	var runCommand = function(command) {
+			console.log(command);
+
+			var child = exec(command, function(error, stdout, stderr) {
+				sys.print('stdout ' + stdout);
+				sys.print('stderr: ' + stderr);
+				if(error != null) {
+					console.log('exec error:' + error);
+					return false;
+				}
+				return true;
+			});
+		};
+
+	var takeImg = function() {
+			runCommand("gphoto2 --capture-image-and-download");
+
+		};
+
+	var scaleImg = function(path, name) {
+			runCommand("convert " + path + name + " -geometry 1000x " + path + "scaled/" + name);
+		};
+
+	var moveImg = function(path_from_img, path_to_img){
+		runCommand("mv "+ path_from_img+" "+ path_to_img);
+	};
+
+	var get_image_name= function(){
+		// TODO : impl
+		return "tempName.jpg";
+	};
+
 	console.log("connnect");
 	socket.on('disconnect', function(socket) {
 		console.log("disconnect");
@@ -22,15 +54,11 @@ io.sockets.on('connection', function(socket) {
 	});
 	socket.on('ping', function(data) {
 		console.log(data.txt);
-		var child = exec("gphoto2 --capture-image-and-download", function(error, stdout, stderr) {
-			sys.print('stdout ' + stdout);
-			sys.print('stderr: ' + stderr);
-			if(error != null) {
-				console.log('exec error:' + error);
-				return false;
-			}
-			return true;
-		});
+		var img_name=get_image_name();
+		takeImg();
+		resetTimeout(, 5000);
+		moveImg("capt0000.jpg", "image/"+img_name);
+		scaleImg("image/", img_name);
 		socket.emit("pong", {
 
 			txt: "Tar bilde"
