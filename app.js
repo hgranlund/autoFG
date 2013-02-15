c773b4a9581ec851e9a1695f7cfb2dfe6f1ed7c6
spohner <str3sses@gmail.com>
2013-02-15 14:56:03 +0100

    Added simplified imagecommand

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var sys = require('sys')
+var sys = require('sys');
 var exec = require('child_process').exec;
 var child;
 var express = require("express");
@@ -13,7 +13,9 @@ console.log('Listening on port 3000');
 var io = socket.listen(server);
 
 io.sockets.on('connection', function(socket) {
-	var runCommand = function(command) {
+	console.log('connected to client');
+	var recordImg = function(path_from_img, path_to_img, path, name) {
+			var command = "gphoto2 --capture-image-and-download && mv "+ path_from_img+" "+ path_to_img+" && convert " + path + name + " -geometry 1000x " + path + "scaled/" + name;
 			console.log(command);
 
 			var child = exec(command, function(error, stdout, stderr) {
@@ -27,43 +29,15 @@ io.sockets.on('connection', function(socket) {
 			});
 		};
 
-	var takeImg = function() {
-			runCommand("gphoto2 --capture-image-and-download");
-
-		};
-
-	var scaleImg = function(path, name) {
-			runCommand("convert " + path + name + " -geometry 1000x " + path + "scaled/" + name);
-		};
-
-	var moveImg = function(path_from_img, path_to_img){
-		runCommand("mv "+ path_from_img+" "+ path_to_img);
-	};
-
 	var get_image_name= function(){
 		// TODO : impl
 		return "tempName.jpg";
 	};
 
-	console.log("connnect");
-	socket.on('disconnect', function(socket) {
-		console.log("disconnect");
-	});
-	socket.emit("pong", {
-		txt: "Connected to server"
-	});
 	socket.on('ping', function(data) {
 		console.log(data.txt);
 		var img_name=get_image_name();
-		takeImg();
-		resetTimeout(, 5000);
-		moveImg("capt0000.jpg", "image/"+img_name);
-		scaleImg("image/", img_name);
-		socket.emit("pong", {
-
-			txt: "Tar bilde"
-
-		});
+		recordImg("capt0000.jpg", "image/"+img_name, "image/", img_name);
 	});
 });
 
